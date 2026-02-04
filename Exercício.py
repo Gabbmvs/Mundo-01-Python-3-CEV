a853d1032cc19bd3a2b742836a1d448cbea9ab71
Gabriel Marques Vieira dos Santos <152335328+Gabbmvs@users.noreply.github.com>
2026-02-04 18:35:52 -0300

    Update Exercício.py

--- Exercício.py
+++ Exercício.py
@@ -3,7 +3,7 @@ nome = input('Digite seu nome completo: ')
 print('Analisando seu nome...')
 print(f'Seu nome em  maiusculas é: {nome.upper()}')
 print(f'Seu nome em  minusculas é: {nome.lower()}')
-print(f'Seu nome tem  {len(nome.replace (" ", ""))} letras') #que merda hein
+print(f'Seu nome tem  {len(nome.replace (" ", ""))} letras')
 partes = nome.split()
 primeiro_nome = partes[0]
 print(f'Seu primeiro nome é: {primeiro_nome}')
